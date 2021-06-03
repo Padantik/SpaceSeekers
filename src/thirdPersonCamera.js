20339231138ea84bd72e1a72c4a4e7f0591ec0e3
Paddy D'Angelo <73836425+Padantik@users.noreply.github.com>
2021-06-03 18:27:11 +0100

    Delete thirdPersonCamera.js

--- src/thirdPersonCamera.js
+++
@@ -1,9 +0,0 @@
-class ThirdPersonCamera {
-    constructor(params) {
-        this.params = params;
-        this.camera = params.camera
-
-        this.currentPosition = new THREE.Vector3();
-        this.currentLookat = new THREE.Vector3();
-    }
-}
